52a68032929431d82cdb33b79276d5766600518b
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-04 12:38:10 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,9 @@ class Recipe < ActiveRecord::Base
 
   def ingredients_attributes=(ingredients_attributes)
     ingredients_attributes.values.each do |ingredient_attributes| 
-      self.ingredients.build(ingredient_attributes)
+      if ingredient_attributes[:name] != ""
+        self.ingredients.build(ingredient_attributes)
+      end
     end
   end
 
